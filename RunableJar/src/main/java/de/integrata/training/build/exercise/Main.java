494235b15d505b569c0a05ea76f0282e7f97aea2
Stephan Pauxberger <stephan@blackbuild.com>
2015-12-14 21:33:50 +0100

    Initial

---
+++ RunableJar/src/main/java/de/integrata/training/build/exercise/Main.java
@@ -0,0 +1,12 @@
+package de.integrata.training.build.exercise;
+
+import java.io.Console;
+
+public class Main { 
+
+	public static void main(String[] args) {
+		System.out.println("Hallo Welt");
+		System.console().readLine("Press enter to close");
+	}
+
+}
